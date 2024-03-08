16748281b04c841b99cc8dcb0066251b0ffd1dab
Artur Frimu <artur.frimu2@gmail.com>
2024-03-08 15:56:13 +0200

    feat: configure redisConnectionFactory

--- src/main/java/com/arturfrimu/redis/example1/config/RedisConfig.java
+++ src/main/java/com/arturfrimu/redis/example1/config/RedisConfig.java
@@ -1,14 +1,24 @@
-package com.arturfrimu.redis.example1;
+package com.arturfrimu.redis.example1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        var configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6379);
+        return new JedisConnectionFactory(configuration);
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
