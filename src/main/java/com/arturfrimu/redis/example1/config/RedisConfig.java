a18cc4e737785125fda87c5ad7c5bec82436b5e0
Artur Frimu <artur.frimu2@gmail.com>
2024-03-08 17:35:33 +0200

    Adjust RedisConfig.class for Redis needs

--- src/main/java/com/arturfrimu/redis/example1/config/RedisConfig.java
+++ src/main/java/com/arturfrimu/redis/example1/config/RedisConfig.java
@@ -1,41 +1,56 @@
 package com.arturfrimu.redis.example1.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.arturfrimu.redis.example1.entity.Article;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.data.redis.enable-transaction-support:false}")
-    private boolean enableTransactionSupport;
-
     @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-        var configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
-        return new JedisConnectionFactory(configuration);
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("localhost");
+        redisStandaloneConfiguration.setPort(6379);
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(30); // Maximum active connections to Redis instance
+        poolConfig.setMaxIdle(10); // Number of connections to keep in idle state
+        poolConfig.setMinIdle(2); // Minimum number of idle connections in the pool
+        poolConfig.setTestOnBorrow(true); // Test the connection before borrowing from the pool
+        poolConfig.setTestOnReturn(true); // Test the connection before returning to the pool
+
+        var jedisClientConfiguration = JedisClientConfiguration.builder()
+                .usePooling()
+                .poolConfig(poolConfig)
+                .build();
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
-        template.setValueSerializer(new JdkSerializationRedisSerializer());
-        template.setEnableTransactionSupport(enableTransactionSupport);
-        template.afterPropertiesSet();
+    public RedisTemplate<String, Article> redisTemplate() {
+        RedisTemplate<String, Article> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        // Key serializer
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        // Value serializer
+        Jackson2JsonRedisSerializer<Article> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Article.class);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
         return template;
     }
 }
