0b2f41d655b525745c10ff5d81e41740fbfc702b
Artur Frimu <artur.frimu2@gmail.com>
2024-03-08 17:35:14 +0200

    Adjust Article.class for Redis needs

--- src/main/java/com/arturfrimu/redis/example1/entity/Article.java
+++ src/main/java/com/arturfrimu/redis/example1/entity/Article.java
@@ -1,36 +1,43 @@
 package com.arturfrimu.redis.example1.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
-@RedisHash("ARTICLE")
-@NoArgsConstructor
-@Setter
 @Getter
-@Table(name = "ARTICLE")
-@Entity
-public class Article {
+@Setter
+@NoArgsConstructor
+@RedisHash("ARTICLE")
+public class Article implements Serializable {
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
+    private String id;
     private String title;
     private String text;
     private BigDecimal rating;
 
-    public Article(Long id, String title, String text, BigDecimal rating) {
+    public Article(String id, String title, String text, BigDecimal rating) {
         this.id = id;
         this.title = title;
         this.text = text;
         this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id) && Objects.equals(title, article.title) && Objects.equals(text, article.text) && Objects.equals(rating, article.rating);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, text, rating);
     }
 }
